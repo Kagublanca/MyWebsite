f9e50795519e8c601519ae04c9494fff60a7c82f
Kagublanca <reecekagu@gmail.com>
2022-01-22 16:26:51 +0000

    Sorted all the folders / data structure of my build, added another md

---
+++ kaguportfolio/src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my website son!</h1>
+      <h2>I need to make a plan I think first though. Low fi, high fi? Maybe!</h2>
+    </div>
+  );
+}
+
+export default App;
